982170597bbaa907c54eb87b8a528dd2a90ef335
Nikitarathod2001 <nikitarathod3601@gmail.com>
2025-11-12 19:50:55 +0530

    Update TodoList

--- frontend/src/components/TodoItem.jsx
+++ frontend/src/components/TodoItem.jsx
@@ -29,7 +29,7 @@ const TodoItem = ({ todo }) => {
     <div className="flex items-center justify-between w-full max-w-md bg-white border border-gray-300 rounded-xl p-4 shadow-sm hover:shadow-md transition-all duration-200 mb-4">
       <p
         className={` ${
-          todo.completed === "true" ? "line-through text-gray-400" : "text-gray-600"
+          todo.completed ? "line-through text-gray-400" : "text-gray-600"
         }`}
       >
         {todo.task}
@@ -37,12 +37,12 @@ const TodoItem = ({ todo }) => {
       <div className="flex gap-3">
         <button onClick={changeStatus}
           className={`px-3 py-1 rounded-lg text-sm font-semibold transition ${
-            todo.completed === "true"
+            todo.completed
               ? "bg-green-100 text-green-700 hover:bg-green-200"
               : "bg-yellow-100 text-yellow-700 hover:bg-yellow-200"
           }`}
         >
-          {todo.completed === "true" ? "Completed" : "Pending"}
+          {todo.completed ? "Completed" : "Pending"}
         </button>
         <button className="px-3 py-1 bg-red-100 text-red-700 rounded-lg text-sm font-semibold hover:bg-red-200 transition">
           Delete
